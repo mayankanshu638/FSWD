607e0ccfc2d6bbe43cec8670cc1abff13ce2e979
mayankanshu638 <mayankanshuaggarwal26@gmail.com>
2021-03-17 12:40:19 +0530

    Second commit

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-
+$var1 =10;
+$var2 =20;
+$sum = $var1 + $var2;
+echo "Hello World in PHP <br>";
 echo "Hello World in PHP";
-
+echo "<br>Sum of 2 numbers is $sum";
+echo '<br>Sum of 2 numbers is $sum';
 ?>
 
 <html>
@@ -9,7 +13,12 @@ echo "Hello World in PHP";
         <title>PHP First Program</title>
     </head>
     <body>
-        <br>
-    <?php echo "Hello World in PHP"; ?>
+       <form method="GET" action="index.php">
+           Variable 1 <input type="text" name="var1">
+           Variable 2 <input type="text" name="var2">
+           <br>
+           <input type = "submit">
+       </form> 
+   
     </body>
 </html>
