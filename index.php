b069b74a8d1e385abe478ce9c22169b0862b5ba9
mayankanshu638 <mayankanshuaggarwal26@gmail.com>
2021-03-17 12:31:37 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello World in PHP";
+
+?>
+
+<html>
+    <head>
+        <title>PHP First Program</title>
+    </head>
+    <body>
+        <br>
+    <?php echo "Hello World in PHP"; ?>
+    </body>
+</html>
