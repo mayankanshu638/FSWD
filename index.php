6db1ffe6834493e714fce99ddfbdabab1d64c7f6
mayankanshu638 <mayankanshuaggarwal26@gmail.com>
2021-03-24 19:00:13 +0530

    Commit

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
-<?php
-    // $var1 = 10;
-    // $var2 = 20;
-    //$sum = $_GET['var1'] + $_GET['var2'];
-    $sum = $_POST['var1'] + $_POST['var2'];
-    echo "Sum of 2 variables is $sum";
-?>
-
 <html>
     <head>
-        <title>PHP First Program</title>
+        <title>My first page</title>
     </head>
     <body>
-       <form method="POST" action="index.php">
-           Variable 1 <input type="text" name='var1'>
-           Variable 2 <input type="text" name='var2'>
-           <br>
-           <input type = "submit">
-       </form> 
-   
+        <form method="POST" action="index.php">
+            Variable 1 <input type="text" name="var1">
+            Variable 2 <input type="text" name="var2">
+            <input type="submit" name="submit">
+        </form>
     </body>
 </html>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        $sum = $_POST['var1'] + $_POST['var2'];
+        echo "Sum of 2 numbers is $sum";
+    }
+    else
+    {
+        echo "No input";
+    }
+ 
+ ?>
+
